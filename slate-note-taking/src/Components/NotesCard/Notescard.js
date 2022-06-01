bb4f367450f2fbbee5fab7ae9e10cda41a9fea4c
iprankurpandey <pprankur@gmail.com>
2022-06-01 23:06:59 +0530

    added notes card

---
+++ slate-note-taking/src/Components/NotesCard/Notescard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useNoteTakingContext } from "../Context/NotetakingContext";
+import "./Notescard.css";
+function Notescard({ notesData }) {
+  const {
+    inputTextTitleValue,
+    labelRadioBoxValue,
+    priorityRadioBoxValue,
+    textareaBoxValue,
+    notesBgColor,
+    noteCreationTime,
+  } = notesData[0];
+
+  return (
+    <div className="notes-data-container">
+      <div className="notes-card" style={{ backgroundColor: notesBgColor }}>
+        <h2> {inputTextTitleValue}</h2>
+        <p> {textareaBoxValue} </p>
+
+        <div className="chips-container">
+          <span className="material-icons notesmi chips">
+            label
+            <span className="label-text"> {labelRadioBoxValue}</span>
+          </span>
+        </div>
+        <div className="chips-container">
+          <span className="material-icons notesmi chips">
+            priority_high
+            <span className="label-text"> {priorityRadioBoxValue}</span>
+          </span>
+        </div>
+        <sub> created at : {noteCreationTime} </sub>
+        <div className="action-icons">
+          <span className="material-icons notesmi">edit </span>
+          <span className="material-icons notesmi">archive </span>
+          <span className="material-icons notesmi">delete </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Notescard;

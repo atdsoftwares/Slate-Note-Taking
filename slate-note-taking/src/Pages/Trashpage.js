586970370e866639c44cb9cf8272e113541c8f7a
iprankurpandey <pprankur@gmail.com>
2022-06-03 16:51:17 +0530

    updated pages

--- slate-note-taking/src/Pages/Trashpage.js
+++ slate-note-taking/src/Pages/Trashpage.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDeleteNotesContext } from "../Components/Context/ArchiveNotesContext";
+import { useNoteTakingContext } from "../Components/Context/NotetakingContext";
+import { useTrashNotesContext } from "../Components/Context/TrashNotesContext";
 import Footer from "../Components/Footer/Footer";
 import Header from "../Components/Header/Header";
+import TrashNotesCard from "../Components/NotesCard/TrashNotesCard";
 import Sidebar from "../Components/Sidebar/Sidebar";
 
 function Trashpage() {
+  const { getTrashedNotes, getTrashedNotesFn } = useTrashNotesContext();
+
+  useEffect(() => {
+    getTrashedNotesFn();
+  }, []);
+
   return (
     <div>
       <Header />
       <Sidebar />
+      <div>
+        {getTrashedNotes.length <= 0 ? (
+          <h1 className="header-text">
+            {" "}
+            No notes to display in trash page, add some ..!
+          </h1>
+        ) : (
+          <div className="notes-container">
+            {getTrashedNotes.map((trashnotesdata) => (
+              <TrashNotesCard
+                trashnotesdata={trashnotesdata}
+                key={trashnotesdata._id}
+              />
+            ))}
+          </div>
+        )}
+      </div>
       <Footer />
     </div>
   );

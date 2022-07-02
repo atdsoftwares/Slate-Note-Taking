4b738822a45faa2db82a35964ed98d0b56b91950
iprankurpandey <pprankur@gmail.com>
2022-07-02 21:48:14 +0530

    updated user id and password

--- slate-note-taking/src/backend/db/users.js
+++ slate-note-taking/src/backend/db/users.js
@@ -8,10 +8,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshBalika123",
+    firstName: "Prankur",
+    lastName: "Pandey",
+    email: "pprankur@gmail.com",
+    password: "pprankur123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
